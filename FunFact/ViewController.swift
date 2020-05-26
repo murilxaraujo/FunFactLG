0972a3691cd3a5a1ca13c9cf70ba8999d47cb946
Murilo Araujo <muernandes@icloud.com>
2020-05-26 08:36:13 -0300

    Initial Commit

---
+++ FunFact/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunFact
+//
+//  Created by Murilo Araujo on 26/05/20.
+//  Copyright © 2020 LG Sistemas SA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FunFactTests/FunFactTests.swift
@@ -0,0 +1,34 @@
+//
+//  FunFactTests.swift
+//  FunFactTests
+//
+//  Created by Murilo Araujo on 26/05/20.
+//  Copyright © 2020 LG Sistemas SA. All rights reserved.
+//
+
+import XCTest
+@testable import FunFact
+
+class FunFactTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
